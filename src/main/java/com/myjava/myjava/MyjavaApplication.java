e6962f8bde7607de34a74b47ae6e69b6d8b8660a
anGaoLang <xianglei@5gegg.cn>
2020-11-25 17:58:23 +0800

    no message

--- src/main/java/com/myjava/myjava/MyjavaApplication.java
+++ src/main/java/com/myjava/myjava/MyjavaApplication.java
@@ -6,7 +6,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class MyjavaApplication {
     public static void main(String[] args) {
-        System.out.println("123123");
         SpringApplication.run(MyjavaApplication.class, args);
     }
 }
